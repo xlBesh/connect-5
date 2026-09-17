d8f91e0ba4a5a152ab64d2ecd4551a7d9a8a0951
xlBesh <bsooora@gmail.com>
2026-09-17 22:09:42 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = "connect5-v1";
+const CACHE_NAME = "connect5-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
